99d63c65a41f02e591950658205ab161c3830366
inyoung <rmdnps10@gmail.com>
2024-02-08 01:53:15 +0900

    Feat: IntersectionObserveer 을 통해 FirstSection이 intersecting 될 때 헤더 로고, FirstSection 로고 보일지 정하고 로고 색상 정하기 #3

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -17,6 +17,7 @@ function About() {
           setIsVisibleHeaderLogo(true);
         } else {
           setIsHeaderBlack(true);
+          setIsVisibleHeaderLogo(false);
         }
       },
       {
