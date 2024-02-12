f148435023e7d14558b0712a1718b07237cbf490
inyoung <rmdnps10@gmail.com>
2024-02-12 14:29:10 +0900

    Feat: Header에서location 이동할 시 useLoading 훅을 호출 #1

--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -13,8 +13,7 @@ function useLoading() {
       const timer = setTimeout(() => {
         setLoadingProgress((prev) => Math.min(prev + 1, 100)); // 10%씩 증가
       }, 10); // 0.5초마다 업데이트
-
-      return () => clearTimeout(timer); // 컴포넌트가 unmount될 때 타이머 제거
+      return () => clearTimeout(timer);
     } else if (loadingProgress === 100) {
       setTimeout(() => setIsLoading(false), 1000);
     }
