17b8e589b525da55084d1e256dfeec0279c46eb8
inyoung <rmdnps10@gmail.com>
2024-02-14 15:01:09 +0900

    Commit before Merge

--- src/components/Recruit/Finish.jsx
+++ src/components/Recruit/Finish.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../../assets/icon/mark.png';
+import logo from '../../assets/icon/likelion-grey.svg';
 import useStore from './Store';
 import { useNavigate } from 'react-router-dom';
 const Finish = () => {
