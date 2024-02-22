8e439ecf94a9d38209a9125f9abd239411128afe
inyoung <rmdnps10@gmail.com>
2024-02-22 22:03:05 +0900

    Feat: tech stack a href revise

--- src/components/Recruit/Schedule.jsx
+++ src/components/Recruit/Schedule.jsx
@@ -18,13 +18,13 @@ const Schedule = () => {
       title: 'Front-End',
       content: '사용자와 상호작용하는 인터페이스를 시각화하여 구현합니다.',
       more: '기술 스택 보러가기',
-      link: 'https://www.instagram.com/likelion_sg/',
+      link: 'https://www.instagram.com/p/C3fFLXIP6pE/?img_index=4',
     },
     {
       title: 'Back-End',
       content: '데이터 처리, 비즈니스 로직, 서버 관리 등을 담당합니다.',
       more: '기술 스택 보러가기',
-      link: 'https://www.instagram.com/likelion_sg/',
+      link: 'https://www.instagram.com/p/C3fFLXIP6pE/?img_index=5',
     },
   ];
 
